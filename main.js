549ce5b22f32688ba0fa7b96eede5879be2cb4fc
Mitchell Blanchard <mrblanch@jmmassociates.com>
2022-04-05 18:46:42 -0400

    mobile menu

---
+++ main.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const navul = document.getElementById('nav-ul');
+hamburger.addEventListener('click', () => {
+    navul.classList.toggle('show');
+});
